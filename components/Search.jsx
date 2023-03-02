30f69e6bc5bfb69f13a8456f0808f9ddeceb680a
nocturnal_hum <thanin.chan@gmail.com>
2023-03-01 19:53:15 -0500

    Add query UseState for Search input

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 export default function Search() {
+  const [query, setQuery] = useState('');
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+  };
+
   return (
     <div className='container relative m-auto max-w-lg py-6'>
       <form className='flex rounded-xl border p-2 px-4'>
         <input
           type='text'
           placeholder='Search for a location...'
+          value={query}
+          onChange={onChange}
           className='w-full bg-transparent text-xl text-white focus:outline-none'
         />
         <button>
-          <BsSearch size={20} color={'white'} />
+          <BsSearch className='h-5 w-5 text-white' />
         </button>
       </form>
     </div>
